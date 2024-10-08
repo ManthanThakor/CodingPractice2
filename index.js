1e080e8ddcae12b93bd5bc273634f0ed5f12dfd7
ManthanThakor <thakormanthan849@gmail.com>
2024-10-08 22:38:32 +0530

    Str

--- index.js
+++ index.js
@@ -69,20 +69,169 @@ function temp(fahrenheit) {
 
 const tableNum = 10;
 
-function multiply(num) {}
+function multiply(num, range = 10) {
+  console.log(`Table of Number ${num} :`);
+  for (let i = 1; i <= range; i++) {
+    const result = num * i;
+    console.log(`${num} * ${i} = ${result}`);
+  }
+}
+// console.log(multiply(tableNum));
+// Table of Number 10 :
+// 10 * 1 = 10
+// 10 * 2 = 20
+// 10 * 3 = 30
+// 10 * 4 = 40
+// 10 * 5 = 50
+// 10 * 6 = 60
+// 10 * 7 = 70
+// 10 * 8 = 80
+// 10 * 9 = 90
+// 10 * 10 = 100
 
 //! ===============================
-// leap year or not
+// Prime Number OR NOT
+//! ===============================
+
+const primeNum = 17;
+function prime(num) {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= num / 2; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+if (prime(primeNum)) {
+  // console.log(`${primeNum} is a prime number.`);
+} else {
+  // console.log(`${primeNum} is not a prime number.`);
+}
+//17 is a prime number.
+
+//! ===============================
+// Factorial
+//! ===============================
+
+const factNum = 5;
+
+function fact(numb) {
+  if (numb === 0 || numb === 1) {
+    return 1;
+  }
+  return numb * fact(numb - 1);
+}
+// console.log(fact(factNum)); // 120
+
+//! ===============================
+// Even or odd
+//! ===============================
+
+const numbb1 = 19;
+
+function oddEven(num) {
+  if (num % 2 === 0) {
+    console.log(`The given Number ${num} is even number`);
+  } else {
+    console.log(`The given Number ${num} is odd number`);
+  }
+}
+// console.log(oddEven(numbb1)); //The given Number 19 is odd number
+
+//! ===============================
+// Sum of array
+//! ===============================
+
+const numberSumArr = [10, 20, 30, 40, 50];
+
+function numberSum(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+// console.log(`Sum of Array : ${numberSum(numberSumArr)}`); //Sum of Array : 150
+
+//! ===============================
+// reverse the string
+//! ===============================
+
+const str = "Hi, How are You?";
+function revStr(str) {
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result = result + str[i];
+  }
+  return result;
+}
+// console.log(`Reversed Str = ${revStr(str)}`); // Reversed Str = ?uoY era woH ,iH
+
+//! ===============================
+// Find The Maximum Of ARRAY
+//! ===============================
+const maxNumArray = [10, 28, 42, 11, 23];
+
+function maxNum(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let max = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+// console.log(`max numb in array ${maxNum(maxNumArray)}`);// max numb in array 42
+
+//! ===============================
+// Count Vowels in String
+//! ===============================
+
+const Str2= "ManthanRocks"
+
+function Vowels(Str2){
+
+  let VowelsCount = 0;
+  Vowels = "aeiouAEIOU";
+
+  
+}
+//! ===============================
+//
 //! ===============================
 //! ===============================
-// leap year or not
+// *
+// **
+// ***
+// ****
+// *****
 //! ===============================
-//! ===============================
-// leap year or not
-//! ===============================
-//! ===============================
-// leap year or not
-//! ===============================
-//! ===============================
-// leap year or not
-//! ===============================
+
+const numSec = 5;
+
+function printStar(num) {
+  for (let i = 1; i <= num; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+// console.log(printStar(numSec));
+// *
+// **
+// ***
+// ****
+// *****
